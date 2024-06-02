ead030b77e75e8550d9c56be4ff57c68f58f8ebb
WilsonZhang746 <46818760+WilsonZhang746@users.noreply.github.com>
2024-06-02 20:11:58 +0200

    Add files via upload

--- Section4-Advanceddatamanagement.R
+++ Section4-Advanceddatamanagement.R
@@ -1,7 +1,10 @@
 #course: Beginning using R for data analysis
 #by https://www.youtube.com/@rprogramming32
 #Section 4: Advanced data management
+
+
 setwd("d:\\RStatistics-Tutorial")   # to set working directory
+
 ##set colClasses in read.table() to create a data frame
 vartype<-c("character", "character", "character", "character", "character", "numeric","numeric", "numeric","numeric","character")
 grade <- read.table("University-Fullname-full.csv", colClasses=vartype, header=TRUE, sep=",")                                      
@@ -67,7 +70,7 @@ quantile(x, probs=0.7)    #around 70
 
 
 
-##Probability functions
+##Lecture 3.Probability functions
 # generate and plot normal distributed variables 
 library(ggplot2)
 x <- seq(from = -10, to = 10, by = 0.2)
@@ -99,7 +102,11 @@ runif(10)   #same results
 
 
 
-##Character functions
+
+
+
+
+##Lecture 4. Character functions
 #toupper(y)
 toupper("r is easy")  #returns "R IS EASY"
 
@@ -131,20 +138,63 @@ grep("R", c("R","notR","R-programming", "is"))  #returns c(1,2,3)
 #strsplit(Y, split, fixed=FALSE)		#Splits the elements of character vector x at split
 #  If fixed=FALSE, then pattern is a regular expression
 #  If fixed=TRUE, then pattern is a text string
-strsplit("easy", "")   #returns "e" "a" "s" "y"
+#returns a list
+test <- strsplit("easy", "")   #returns "e" "a" "s" "y" 
+test
+
+#unlist to a vector
+test1 <- unlist(test)
+
+#alternative way
+sapply(test,"[",2)
+test1 
 
 
 
 
 
 
-##Using apply function
+
+
+
+
+
+
+
+
+
+
+
+
+
+##Lecture 5. Using apply function
 #using apply() to rows and columns of a matrix
 test <- matrix(rnorm(20), nrow = 4)
 test
 apply(test, 1, mean)    #row mean of a matrix
 apply(test, 2, mean)    #column mean of a matrix
 apply(test, 2, mean, trim = 0.2) #trimmed column mean
+
+
+### apply() function
+#Here, is a basic example showcasing the use of apply() 
+#function along rows as well as columns.
+# create sample data
+sample_matrix <- matrix(C<-(1:10),nrow=3, ncol=10)
+
+print( "sample matrix:")
+sample_matrix
+
+# Use apply() function across row to find sum
+print("sum across rows:")
+apply( sample_matrix, 1, sum)
+
+# use apply() function across column to find mean
+print("mean across columns:")
+apply( sample_matrix, 2, sum)
+
+
+
 
 
 
@@ -196,7 +246,7 @@ grade
 
 
 
-##Control flow
+##Lecture 6. Control flow
 #for loop
 for (i in 1:3) print("I love R")
 #while loop
@@ -269,7 +319,7 @@ for (i in countries) {
 
 
 
-##creating your own functions
+##Lecture 7. creating your own functions
 #to create a function for statistics
 #mad() to calculate the median of the absolute deviation from median
 sumfunc <- function(x, par = TRUE, print = FALSE) {
@@ -301,6 +351,11 @@ y <- sumfunc(x, print=TRUE)
 
 
 
+
+
+
+
+
 ##Transposing a dataset
 test <- grade[1:5, 7:9]
 test
@@ -312,7 +367,7 @@ t(test)
 
 
 
-## Aggregating data 
+## Lecture 8. Aggregating data 
 setwd("d:\\RStatistics-Tutorial")   # to set working directory
 #create a grade data frame
 vartype<-c("character", "character", "character", "character", "character", "numeric","numeric", "numeric","numeric","character")
@@ -321,8 +376,11 @@ grade
 test<-grade[,c(4,5,7:9)]
 test$Gender<-as.factor(test$Gender)
 test$Country<-as.factor(test$Country)
+test
 agg <- aggregate(test, by = list(test$Gender, 
      test$Country), FUN = mean, na.rm = TRUE)
+
+agg
 
 #a better solution
 agg <- aggregate(test[-c(1,2)], 
@@ -346,6 +404,12 @@ agg
 
 
 
+### Lecture 9. Descriptive statistics
+setwd("d:\\RStatistics-Tutorial")   # to set working directory
+#create a grade data frame
+vartype<-c("character", "character", "character", "character", "character", "numeric","numeric", "numeric","numeric","character")
+grade <- read.table("University-Fullname-full.csv", colClasses=vartype, header=TRUE, sep=",")                                      
+grade
 ##Descriptive statistics with summary()
 vars <- c("Math", "Physics", "Chemistry")
 summary(grade[vars])
@@ -369,7 +433,7 @@ describe(grade[vars])
 
 
 
-### Lecture 13. Using sample() to generate a set of sample data
+### Lecture 10. Using sample() to generate a set of sample data
 
 #Example 1:
 
@@ -379,21 +443,17 @@ x <- sample(1:1000, 20, replace=TRUE)
 
 print(x)
 
-
 #example 2
 #sample range lies between 1 to 5
 x<- sample(1:5)
 #prints the samples
 x
 
-
-
 #example 3
 #sample n numbers, where n larger than length of source vector
 x<- sample(1:5,10,replace=TRUE)
 #prints the samples
 x
-
 
 
 #set.seed() - set.seed function will produce the same sequence 
@@ -410,13 +470,11 @@ row_vec
 print(mtcars[row_vec, ])
 
 
-
 set.seed(1234)
 row_vec <- sample(1:nrow(mtcars), 20, replace = TRUE)
 
 row_vec
 print(mtcars[row_vec, ])
-
 
 
 #example 5
@@ -427,17 +485,6 @@ sample (c('Good','Defective'), size=10, replace=T, prob=c(.80,.20))
 
 
 
-#sample n numbers, where n larger than length of source vector
-x<- sample(1:5,10,replace=TRUE)
-#prints the samples
-x
-
-
-set.seed(1234)
-row_vec <- sample(1:nrow(mtcars), 20, replace = TRUE)
-
-row_vec
-print(mtcars[row_vec, ])
 
 
 
@@ -449,83 +496,7 @@ print(mtcars[row_vec, ])
 
 
 
-
-
-
-
-
-
-
-### Lecture 14. table function family
-
-library(vcd)
-head(Arthritis)
-
-# one way table
-mytable <- table(Arthritis$Improved)
-mytable                       # counts
-prop.table(mytable)           # proportions
-prop.table(mytable)*100       # percents
-
-# two way table
-mytable <- xtabs(~ Treatment+Improved, data=Arthritis)
-mytable  # counts
-
-margin.table(mytable, 1)    # total counts for Treatment 
-prop.table(mytable, 1)      # row proportions (rows add to 1)
-
-margin.table(mytable, 2)    # total counts for Improved
-prop.table(mytable, 2)      # column proportions (columns add to 1)
-
-prop.table(mytable)         # cell proportions (all cells add to 1)
-addmargins(mytable)         # cell counts with row and column sums
-addmargins(prop.table(mytable)) # cell proportions with row and column proportions
-
-addmargins(prop.table(mytable, 1), 2) # row proportions with row sums
-addmargins(prop.table(mytable, 2), 1) # column proportions with column sums
-
-# Listing 7.8 Two-way table using CrossTable
-library(gmodels)
-CrossTable(Arthritis$Treatment, Arthritis$Improved)
-
-# Listing 7.9 Three-way contingency table
-mytable <- xtabs(~ Treatment+Sex+Improved, data=Arthritis) 
-mytable          
-margin.table(mytable, 1)  # totals for Treatment
-margin.table(mytable, 2)  # totals for Sex
-margin.table(mytable, 3)  # totals for Improved
-margin.table(mytable, c(1, 3)) # totals for Treatment by Improved
-
-# Treatment by Sex for each Level of Improved
-ftable(mytable)
-ftable(prop.table(mytable, c(1, 2))) # proportions sum to 1 over index omitted
-ftable(addmargins(prop.table(mytable, c(1, 2)), 3)) 
-ftable(addmargins(prop.table(mytable, c(1, 2)), 3)) * 100
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-### Lecture 15. Data Reshaping
+### Lecture 11. Data Reshaping
 
 # Transpose of a Matrix using t() 
 matrix1 <- matrix(c(1:20), nrow=5, byrow=TRUE)
@@ -618,4 +589,203 @@ head(aqw)        #wide form again
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+### Lecture 14. table function family
+
+library(vcd)
+head(Arthritis)
+
+# one way table
+mytable <- table(Arthritis$Improved)
+mytable                       # counts
+prop.table(mytable)           # proportions
+prop.table(mytable)*100       # percents
+
+# two way table
+mytable <- xtabs(~ Treatment+Improved, data=Arthritis)
+mytable  # counts
+
+margin.table(mytable, 1)    # total counts for Treatment 
+prop.table(mytable, 1)      # row proportions (rows add to 1)
+
+margin.table(mytable, 2)    # total counts for Improved
+prop.table(mytable, 2)      # column proportions (columns add to 1)
+
+prop.table(mytable)         # cell proportions (all cells add to 1)
+addmargins(mytable)         # cell counts with row and column sums
+addmargins(prop.table(mytable)) # cell proportions with row and column proportions
+
+addmargins(prop.table(mytable, 1), 2) # row proportions with row sums
+addmargins(prop.table(mytable, 2), 1) # column proportions with column sums
+
+# Listing 7.8 Two-way table using CrossTable
+library(gmodels)
+CrossTable(Arthritis$Treatment, Arthritis$Improved)
+
+# Listing 7.9 Three-way contingency table
+mytable <- xtabs(~ Treatment+Sex+Improved, data=Arthritis) 
+mytable          
+margin.table(mytable, 1)  # totals for Treatment
+margin.table(mytable, 2)  # totals for Sex
+margin.table(mytable, 3)  # totals for Improved
+margin.table(mytable, c(1, 3)) # totals for Treatment by Improved
+
+# Treatment by Sex for each Level of Improved
+ftable(mytable)
+ftable(prop.table(mytable, c(1, 2))) # proportions sum to 1 over index omitted
+ftable(addmargins(prop.table(mytable, c(1, 2)), 3)) 
+ftable(addmargins(prop.table(mytable, c(1, 2)), 3)) * 100
+
+
+
+
+
+##Lecture. Environment and Scoping rule in R
+
+library(pryr)
+obj1 <- c(1,2,3)
+obj2 <- c("one","two","three")
+
+library(ggplot2)
+setwd("d:\\RStatistics-Tutorial")   # to set working directory
+##set colClasses in read.table() to create a data frame
+vartype<-c("numeric","numeric" ,"character", "character", "character", "character", "numeric","numeric", "character")
+Income <- read.table("Income.csv", colClasses=vartype, header=TRUE, sep=",")                                      
+head(Income)
+
+Income$Race<-as.factor(Income$Race)
+Income$Gender<-as.factor(Income$Gender)
+Income$Region<-as.factor(Income$Region)
+Income$Married<-as.factor(Income$Married)
+Income$Job<-as.factor(Income$Job)
+
+# --  Creating a graph with ggplot2
+library(ggplot2)
+
+## Start building graphs
+
+#Add geoms_ in ggplot()
+#add a scatter plot of Years and Salary
+ggplot(data = Income, mapping = aes(x = Years, y = Salary)) + 
+  +   geom_point()
+
+
+
+states <- as.data.frame(state.x77[,c("Murder", "Population",
+                                     "Illiteracy", "Income", "Frost")])
+head(states)
+
+fit <- lm(Murder ~ Population + Illiteracy + Income + Frost, data=states)
+
+
+#R environment
+
+environment()    #active environment
+ls(globalenv())   #list object in current env
+ls(emptyenv())
+
+parenvs(all=TRUE) 
+
+#example of function scope environment
+aggre <- function(a,b) {
+  a + b
+  }
+multi_aggre <- function (a,b,n) {
+  aggre(a,b) * n
+  }
+multi_aggre (1,2,3)
+
+
+
+par_child <- function(a,b){
+  list(current = environment(),
+       par = parent.env(environment()),
+       obj = ls.str(environment()))
+}
+par_child(1,2)
+
+
+
+
+
+
+
+
+
+## R S3 class
+# Transpose of a Matrix using t() 
+mtx1 <- matrix(c(1:6), nrow=2, byrow=TRUE)
+print("Matrix 1 is")
+print(mtx1)
+
+attributes(mtx1)
+class(mtx1)
+names(mtx1) <- c('1','2','3','4','5','6')
+attributes(mtx1)
+
+
+
+df1 <- data.frame(mtx1)
+
+df1
+
+vec1 <-  c("A", "B", "A","B","A","B")
+attr(df1, "images") <-vec1
+attributes(df1)
+
+attr(df1, "images")
+class(df1)
+
+#generic function and methods
+print.data.frame
+
+print.factor
+
+print.matrix   #not exist
+
+#to show methods with generic function 'print',
+#associated with different class
+methods(print)
+
+#to show methods for class 'factor', with corresponding
+#generic functions
+methods(class="factor")
+
+#to show methods for class 'data.frame', with corresponding
+#generic functions
+methods(class="data.frame")
+
+methods(class="matrix")
+
+
+#set new class for df1
+
+class(df1)<- 'test'
+
+print(df1)
+
+print.test <- function(x) {
+  print("this is a test")
+  
+ 
+}
+
+print(df1)
 
